c66fba126907e5a228fa7d620282ec65ce5b1cf1
John-Tonny <szlhtao@tom.com>
2019-06-20 14:17:22 +0800

    convert

--- electrumx/lib/hash.py
+++ electrumx/lib/hash.py
@@ -77,8 +77,7 @@ def hash_to_hex_str(x):
 
 def hex_str_to_hash(x):
     '''Convert a displayed hex string to a binary hash.'''
-    return bytes(hex_to_bytes(x))
-    #return bytes(reversed(hex_to_bytes(x)))
+    return bytes(reversed(hex_to_bytes(x)))
 
 
 class Base58Error(Exception):
